4267c4ac49e949f6b0b15b172172f0cde9b85b02
Julien Danjou <julien@danjou.info>
2016-01-06 16:25:39 +0100

    Add help usage
    Signed-off-by: Julien Danjou <julien@danjou.info>

--- tests/utils.sh
+++ tests/utils.sh
@@ -4,5 +4,9 @@ source lib/_utils
 testParseOptions ()
 {
     overtest_parse_options foobar
+    assertEquals 2 $?
+    overtest_parse_options -h
+    assertEquals 1 $?
+    overtest_parse_options --help
     assertEquals 1 $?
 }
