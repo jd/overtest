9721b6c739572ecd2fcf353f70e317b3dc1b47fc
Julien Danjou <julien@danjou.info>
2016-01-19 13:44:02 +0100

    influxdb: create a database

--- tests/influxdb.sh
+++ tests/influxdb.sh
@@ -4,13 +4,14 @@ source bin/overtest influxdb
 testEnvironment ()
 {
     assertEquals 51234 ${OVERTEST_INFLUXDB_PORT}
+    assertEquals test ${OVERTEST_INFLUXDB_DATABASE}
     assertEquals influxdb ${OVERTEST_DAEMON}
-    assertEquals "influxdb://localhost:51234" ${OVERTEST_INFLUXDB_URL}
+    assertEquals "influxdb://localhost:51234/test" ${OVERTEST_INFLUXDB_URL}
     assertEquals ${OVERTEST_URL} ${OVERTEST_INFLUXDB_URL}
 }
 
 
 testClient ()
 {
-    influx -port $OVERTEST_INFLUXDB_PORT -execute 'create database test'
+    influx -port $OVERTEST_INFLUXDB_PORT -execute 'SHOW DATABASES;'
 }
