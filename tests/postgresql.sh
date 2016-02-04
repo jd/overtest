dc97ca9adf23b9c83252e0120bacc31f13d7d660
Julien Danjou <julien@danjou.info>
2016-02-04 10:21:12 +0100

    postgresql: set host to localhost
    While this is not necessary in theory, some library like urlparse in
    Python does not like having it empty.

--- tests/postgresql.sh
+++ tests/postgresql.sh
@@ -5,7 +5,7 @@ testEnvironment ()
 {
     assertEquals 9824 ${OVERTEST_POSTGRESQL_PORT}
     assertEquals postgresql ${OVERTEST_DAEMON}
-    assertEquals "postgresql:///template1?host=${OVERTEST_POSTGRESQL_DATA}&port=${OVERTEST_POSTGRESQL_PORT}" ${OVERTEST_POSTGRESQL_URL}
+    assertEquals "postgresql://localhost/template1?host=${OVERTEST_POSTGRESQL_DATA}&port=${OVERTEST_POSTGRESQL_PORT}" ${OVERTEST_POSTGRESQL_URL}
     assertEquals ${OVERTEST_URL} ${OVERTEST_POSTGRESQL_URL}
 }
 
